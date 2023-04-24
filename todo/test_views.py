17374bf4ed61f23de3eb2d79934c5ea1ad984420
stephenbeese <stephenbeese1@live.co.uk>
2023-04-24 10:19:39 +0000

    Add coverage testing for all pages and get to 100%

--- todo/test_views.py
+++ todo/test_views.py
@@ -33,10 +33,16 @@ class TestViews(TestCase):
         existing_items = Item.objects.filter(id=item.id)
         self.assertEqual(len(existing_items), 0)
 
-
     def test_can_toggle_item(self):
         item = Item.objects.create(name='Test Todo Item', done=True)
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item', done=True)
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
