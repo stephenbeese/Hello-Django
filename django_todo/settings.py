bf03a889bf234b1e8fccc66e50b23829f9712b7f
stephenbeese <stephenbeese1@live.co.uk>
2023-04-25 15:02:31 +0000

    Add template location to settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'django_todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['todo/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
