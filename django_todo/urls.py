6d78d9d293496d69644af568eddf7982d4b59226
stephenbeese <stephenbeese1@live.co.uk>
2023-04-25 14:51:34 +0000

    Change todo list name in urls.py

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -20,7 +20,7 @@ from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.get_todo_list, name='get_todo_list'),
+    path('', views.todo_list, name='todo_list'),
     path('add', views.add_item, name='add'),
     path('edit/<item_id>', views.edit_item, name='edit'),
     path('toggle/<item_id>', views.toggle_item, name='toggle'),
