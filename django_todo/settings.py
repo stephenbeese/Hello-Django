c56dbcf97df9cdc6b2af916f33f1e3109f3bc063
stephenbeese <stephenbeese1@live.co.uk>
2023-04-25 20:46:53 +0000

    Chaneg DATABASES object in settings.py file

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -87,8 +87,8 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    # 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    'default': 'postgres://foatextmcilasi:dd48efccc0a4bb9f18b058dadd927eec3f957e25db430618ea890fbe9b2831ee@ec2-44-215-1-253.compute-1.amazonaws.com:5432/d71c87ssiog8r2'
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    # 'default': 'postgres://foatextmcilasi:dd48efccc0a4bb9f18b058dadd927eec3f957e25db430618ea890fbe9b2831ee@ec2-44-215-1-253.compute-1.amazonaws.com:5432/d71c87ssiog8r2'
 }
 
 # Password validation
