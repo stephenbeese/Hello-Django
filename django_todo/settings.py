fbc4652bf9c3ce233203d8090cb81a443769897e
stephenbeese <stephenbeese1@live.co.uk>
2023-04-25 14:43:29 +0000

    Turn debug to True

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ycrtg%sjo(t1%wm$_87#uk*a5!m$l1ho9b7%b29opc07fow0i&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ckz91234-hello-django-todo-app.herokuapp.com']
 
